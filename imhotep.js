223c521667bcd66dce41bdc0ab060cafdda7650b
ccgauche <gaucheron.laurent@gmail.com>
2020-11-26 23:02:05 +0100

    Added the imhotep website

---
+++ imhotep.js
@@ -0,0 +1,50 @@
+document.head.innerHTML = `
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empire constitutionnel très démocratique populaire et républicain monarchiste d'Imhotep</title>
+    <link rel="shortcut icon" href="https://yt3.ggpht.com/ytc/AAUvwngii9rEZlJEU37DqlZojM6t9ucb4-ZowDadugNqjg=s88-c-k-c0x00ffffff-no-rj" type="image/jpg">
+`;
+document.body.innerHTML = `
+<style>
+        html, body {
+            margin: 0;
+            font-family: sans-serif;
+            background-color: rgb(250, 250, 250);
+        }
+
+        header {
+            display: flex;
+            flex-direction: row;
+            padding: 1rem 2rem;
+            background-color: #FFF;
+            box-shadow: #555 1px 1px 5px;
+        }
+
+        header > .title {
+            font-size: 1.2rem;
+            font-weight: bold;
+            padding-left: 1rem;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-evenly;
+        }
+
+        .container {
+            max-width: 1000px;
+            margin: 0 auto;
+        }
+    </style>
+    <header>
+        <img src="https://yt3.ggpht.com/ytc/AAUvwngii9rEZlJEU37DqlZojM6t9ucb4-ZowDadugNqjg=s88-c-k-c0x00ffffff-no-rj" alt="">
+        <div class="title">
+            <a>EMPIRE CONSTITUTIONNEL TRES DEMOCRATIQUE</a>
+            <a>POPULAIRE ET REPUBLICAIN</a>
+            <a>MONARCHISTE D'IMHOTEP</a>
+        </div>
+    </header>
+    <div class="container">
+        <h1>Ce site a été fermé par l'ONU!</h1>
+        <p>L'ONU comdame l'accès a ce site pour non respect de la déclaration universelle des droits de l'homme et la violations de tous les traités internationnaux existants!</p>
+    </div>
+`;
+console.log("TEST")
